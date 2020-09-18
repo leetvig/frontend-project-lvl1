75bba249140d974b6126eb46b75f747f229f65e1
Leetvig <leetvig@gmail.com>
2020-09-18 17:56:38 +0500

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,10 +4,9 @@ import launchGameEngine from '../index.js';
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  const positiveNum = num > 0 ? num : -num;
-  if (positiveNum < 2) return false;
-  for (let i = 2; i <= positiveNum / 2; i += 1) {
-    if (positiveNum % i === 0) {
+  if (Math.abs(num) < 2) return false;
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (Math.abs(num % i) === 0) {
       return false;
     }
   }
